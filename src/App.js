ca9bd7ee7e9d074d1447b013285543d530fe6bc5
sod3 <mudassir.muneer123@gmail.com>
2025-01-10 21:24:09 +0500

    frontend completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import ChatApp from './Pages/Chat/Chat';
-
 function App() {
   return (
     <div>
--- src/Pages/Chat/Chat.js
+++ src/Pages/Chat/Chat.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Chat.css";
+// Importing icons from react-icons
+import { FaPhone, FaVideo, FaEllipsisV } from "react-icons/fa";
 
 const Chat = () => {
     return (
@@ -10,17 +12,35 @@ const Chat = () => {
                     <input type="text" placeholder="Search..." />
                 </div>
                 <div className="section">
-                    <h2 className="section-title">Lectures</h2>
+                    <h2 className="section-title">Teachers</h2>
                     <ul className="list">
                         <li>
-                            <div className="list-item-name">DR JG</div>
-                            <div className="list-item-name">Today, 9:52pm</div>
-                            <div className="list-item-name">Maam Zoha</div>
-                            <div className="list-item-name">Yesterday, 12:31pm</div>
-                            <div className="list-item-name">Sir Twaha Minai</div>
-                            <div className="list-item-name">Wednesday, 9:12pm</div>
-                            <div className="list-item-name">Sir Zeeshan</div>
-                            <div className="list-item-name">Tuesday, 10:23pm</div>
+                            <img src="/images.jpg" alt="DR JG" className="list-item-avatar" />
+                            <div className="list-item-info">
+                                <div className="list-item-name">DR JG</div>
+                                <div className="list-item-time">Today, 9:52pm</div>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="/images.jpg" alt="Maam Zoha" className="list-item-avatar" />
+                            <div className="list-item-info">
+                                <div className="list-item-name">Maam Zoha</div>
+                                <div className="list-item-time">Yesterday, 12:31pm</div>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="/images.jpg" alt="Sir Twaha Minai" className="list-item-avatar" />
+                            <div className="list-item-info">
+                                <div className="list-item-name">Sir Twaha Minai</div>
+                                <div className="list-item-time">Wednesday, 9:12pm</div>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="/images.jpg" alt="Sir Zeeshan" className="list-item-avatar" />
+                            <div className="list-item-info">
+                                <div className="list-item-name">Sir Zeeshan</div>
+                                <div className="list-item-time">Tuesday, 10:23pm</div>
+                            </div>
                         </li>
                     </ul>
                 </div>
@@ -29,12 +49,25 @@ const Chat = () => {
                     <h2 className="section-title">Students</h2>
                     <ul className="list">
                         <li>
-                            <div className="list-item-name">Mudassir Muneer</div>
-                            <div className="list-item-name">Today, 9:52pm</div>
-                            <div className="list-item-name">Huzaifa Soomar</div>
-                            <div className="list-item-name">Today, 12:11pm</div>
-                            <div className="list-item-name">Syed owais bin najam</div>
-                            <div className="list-item-name">Yesterday, 10:23pm</div>
+                            <img src="/images.jpg" alt="Mudassir Muneer" className="list-item-avatar" />
+                            <div className="list-item-info">
+                                <div className="list-item-name">Mudassir Muneer</div>
+                                <div className="list-item-time">Today, 9:52pm</div>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="/images.jpg" alt="Huzaifa Soomar" className="list-item-avatar" />
+                            <div className="list-item-info">
+                                <div className="list-item-name">Huzaifa Soomar</div>
+                                <div className="list-item-time">Today, 12:11pm</div>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="/images.jpg" alt="Syed Owais Bin Najam" className="list-item-avatar" />
+                            <div className="list-item-info">
+                                <div className="list-item-name">Syed Owais Bin Najam</div>
+                                <div className="list-item-time">Yesterday, 10:23pm</div>
+                            </div>
                         </li>
                     </ul>
                 </div>
@@ -52,52 +85,61 @@ const Chat = () => {
                             <p className="user-status">Online - Last seen, 2:02pm</p>
                         </div>
                     </div>
+                    {/* New Icons Section */}
+                    <div className="chat-header-icons">
+                        <FaPhone className="header-icon" />
+                        <FaVideo className="header-icon" />
+                        <FaEllipsisV className="header-icon" />
                     </div>
-                    <div className="chat-messages">
-                        <div className="message left">
-                            <div className="bubble-left">
-                                <p>Hey There!</p>
-                            </div>
-                            <div className="bubble-left">
-                                <p>How are you?</p>
-                            </div>
-                              <span className="message-time">Today, 8:30pm</span>
-                            </div>
-                            <div className="message right">
-                                <div className="bubble-right">
-                                    <p>Hello!</p>
-                            </div>
-                              <span className="message-time">Today, 8:33pm</span>
-                            </div>
+                </div>
+                <div className="chat-messages">
+                    <div className="message left">
+                        <div className="bubble-left">
+                            <p>Hey There!</p>
+                        </div>
+                        <span className="message-time">Today, 8:30pm</span>
+                    </div>
+                    <div className="message left">
+                        <div className="bubble-left">
+                            <p>How are you?</p>
+                        </div>
+                        <span className="message-time">Today, 8:31pm</span>
+                    </div>
+                    <div className="message right">
+                        <div className="bubble-right">
+                            <p>Hello!</p>
+                        </div>
+                        <span className="message-time">Today, 8:33pm</span>
+                    </div>
 
-                            <div className="message right">
-                                <div className="bubble-right">
-                                    <p>I'm good, how are you?</p>
-                                </div>
-                                <span className="message-time">Today, 8:34pm</span>
-                            </div>
+                    <div className="message right">
+                        <div className="bubble-right">
+                            <p>I'm good, how are you?</p>
+                        </div>
+                        <span className="message-time">Today, 8:34pm</span>
+                    </div>
 
-                            <div className="message left">
-                                <div className="bubble-left">
-                                    <p>I am doing well, Can we meet</p>
-                                </div>
-                                  <span className="message-time">Today, 8:36pm</span>
-                                </div>
+                    <div className="message left">
+                        <div className="bubble-left">
+                            <p>I am doing well, Can we meet?</p>
+                        </div>
+                        <span className="message-time">Today, 8:36pm</span>
+                    </div>
 
-                            <div className="message right">
-                                <div className="bubble-right">
-                                    <p>Yes Sure!</p>
-                                </div>
-                                <span className="message-time">Today, 8:58pm</span>
-                                </div>
-                            </div>
+                    <div className="message right">
+                        <div className="bubble-right">
+                            <p>Yes, sure!</p>
+                        </div>
+                        <span className="message-time">Today, 8:58pm</span>
+                    </div>
+                </div>
 
-                            <div className="chat-input-box">
-                                <input type="text" placeholder="Type your message..." />
-                                <button className="send-btn">Send</button>
-                                </div>  
-                              </div>
-                            </div>
+                <div className="chat-input-box">
+                    <input type="text" placeholder="Type your message..." />
+                    <button className="send-btn">Send</button>
+                </div>
+            </div>
+        </div>
     );
 };
 
