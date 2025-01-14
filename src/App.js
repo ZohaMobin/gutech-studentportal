28b844980d09fe6c85d1371d258d10170d226019
musababbasi450 <musab>
2025-01-14 21:18:37 +0500

    commit by Musab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from "../src/Pages/Navbar/Navbar";
 import Sidebar from "../src/Pages/Sidebar/sidebar/index";
 import Assignments from './Pages/assignments/assignments';
 
@@ -9,7 +10,11 @@ import Dashboard from './Pages/dashboard/dashboard';
 function App() {
   return (
     <Router>
+      <div>
+        <div classname= "placement" ><Navbar /> </div>
+      
       <div className="App">
+      
         <Sidebar />
         <Routes>
           <Route path="/" element={<Dashboard/>} /> 
@@ -18,6 +23,8 @@ function App() {
         </Routes>
        
       </div>
+      </div>
+      
   
     </Router>
   );
