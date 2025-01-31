692e39273e36e3b4d42fd6d96c1e5dc139a22997
99ahsanahmed <ahsan.ha58@gmail.com>
2025-01-31 14:58:31 +0500

    changes before merge

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "../src/Pages/Sidebar/sidebar/index";
-import Assignments from './Pages/assignments/assignments';
+import Assignments from "./Pages/assignments/assignments";
 
-import Dashboard from './Pages/dashboard/dashboard';
-import { Topbar } from './Pages/topbar/topbar';
+import Dashboard from "./Pages/dashboard/dashboard";
+import { Topbar } from "./Pages/topbar/topbar";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Sidebar />
-        <div className='mainContent '>
-  <Topbar/>
-        <Routes>
-
-          <Route path="/" element={<Dashboard/>} /> 
-          <Route path="/assignments" element={<Assignments />} />
-      
-        </Routes>
+        <div className="mainContent ">
+          <Topbar />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/assignments" element={<Assignments />} />
+          </Routes>
         </div>
       </div>
     </Router>
@@ -26,6 +24,3 @@ function App() {
 }
 
 export default App;
-
-
-
